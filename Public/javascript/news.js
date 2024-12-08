d1bc980726623f89a23d30dda35f076b65cce98c
iverson <105322573+anontriton@users.noreply.github.com>
2024-12-07 16:17:00 -0800

    news market and trending pages

---
+++ Public/javascript/news.js
@@ -0,0 +1,24 @@
+const apiKey = 'zuc9hftGHcs7gtlHaKpInxqQvkdRTEeT2e97mncO';
+const endpoint = `https://api.marketaux.com/v1/news/all?api_token=${apiKey}&filter_entities=true&countries=us&limit=1`;
+// API limits to 100 requests for the month
+
+async function fetchNews() {
+    try {
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        const articlesContainer = document.getElementById('articles-container');
+        data.data.forEach(article => {
+            const articleDiv = document.createElement('div');
+            articleDiv.classList.add('article');
+            articleDiv.innerHTML = `
+            <h3><a href="${article.url}" target="_blank">${article.title}</a></h3>
+            <p>${article.description}</p>
+            `;
+            articlesContainer.appendChild(articleDiv);
+        });
+    } catch (error) {
+        console.error('Error fetching news:', error);
+    }
+}
+
+fetchNews();
